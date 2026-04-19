a6629ae07c250ebf22da69c31836022282cf28df
BakhouddinGayratov <gayratovbakhouddin@gmail.com>
2026-04-19 13:16:55 +0500

    Second Commit

--- school_project/settings.py
+++ school_project/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = 'school_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "school_db",
+        "USER": "postgres",
+        "PASSWORD": "admin",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
